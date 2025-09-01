4711fc501a1ad7a71b41308a6a5bd018c2755587
elena-risteska <elena.risteska003@gmail.com>
2025-09-01 14:49:27 +0200

    add type in location model

--- models/Location.js
+++ models/Location.js
@@ -3,8 +3,21 @@ import mongoose from "mongoose";
 const locationSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  images: [{ type: String }], // URLs or file names
-  city: { type: String, required: true },
+  images: [{ type: String }],
+  place: { type: String, required: true },
+  type: {
+    type: String,
+    required: true,
+    enum: [
+      "waterfall",
+      "archeology",
+      "cave",
+      "canyon",
+      "landmark",
+      "lake",
+      "rarity",
+    ],
+  },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
 });
